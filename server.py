85a0a8a0a7ef268001a3096e1a284d86de4d40f3
Cameron Smith <34502005+NorthernL1ghts@users.noreply.github.com>
2023-05-23 12:52:26 +0100

    Add camera_id functionality to WebSocket communication.
    This commit introduces changes to the server and client code to incorporate camera_id functionality in the WebSocket communication.

--- server.py
+++ server.py
@@ -5,30 +5,42 @@ connections = {}
 
 async def handle_client(websocket, path):
     try:
-        # Store the connection in the dictionary
-        connections[id(websocket)] = websocket
+        # Store the connection in the dictionary with default camera_id -1
+        connections[websocket] = -1
 
         while True:
             message = await websocket.recv()
             print(f"Received message from client: {message}")
 
-            # Process the received message here
-            # ...
+            if message.startswith("SET_CAMERA"):
+                # Parse the camera_id from the message
+                camera_id = int(message.split(":")[1])
 
-            response = "Server processed the message"
-            await websocket.send(response)
-            print(f"Sent response to client: {response}")
+                # Update the camera_id for the specific client connection
+                connections[websocket] = camera_id
+                print(f"Client {websocket.remote_address} set camera_id to {camera_id}")
+            elif message == "CLOSE":
+                # Remove the connection from the dictionary and close the WebSocket connection
+                del connections[websocket]
+                await websocket.close()
+                print(f"Closed connection with client {websocket.remote_address}")
+            else:
+                # Process the received message (image data) and camera_id here
+                camera_id = connections[websocket]
+                if camera_id != -1:
+                    # Perform image prediction/detection based on the camera_id
+                    # ...
 
-            # Write the received message and response to files
-            with open("received_messages.txt", "a") as received_file:
-                received_file.write(message + "\n")
-            with open("sent_responses.txt", "a") as response_file:
-                response_file.write(response + "\n")
+                    response = "Server processed the image"
+                    await websocket.send(response)
+                    print(f"Sent response to client {websocket.remote_address}: {response}")
+                else:
+                    print(f"Client {websocket.remote_address} has not set a camera_id")
 
     except websockets.exceptions.ConnectionClosedOK:
-        print("Client connection closed.")
+        print(f"Client connection closed: {websocket.remote_address}")
         # Remove the connection from the dictionary
-        del connections[id(websocket)]
+        del connections[websocket]
 
 async def start_server():
     server = await websockets.serve(handle_client, 'localhost', 8765)
